d9503af2d2d2196d11ade841ac6f6946c1f37d95
waitold <chakkggm@gmail.com>
2019-10-18 02:25:34 +0800

    処理かいた

--- keygame/urls.py
+++ keygame/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.init_key_game, name='init_key_game'),
+    path('', views.key_game, name='init_key_game'),
 ]
--- keygame/views.py
+++ keygame/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import random
 
 # Create your views here.
 
 
-def init_key_game(request):  # セッションに正解の値を持たせる
-    request.session['correct_num'] = random.randrange(10)
-    return render(request, "keygame/key_init.html")
+def key_game(request):
 
+    if request.session.get('correct_num') is None:  # セッションを持っていなければゲームを初期化
+        request.session['correct_num'] = random.randrange(10)
+        return render(request, 'keygame/key_init.html')
 
-def check_answer(request): # postの内容を取得し正誤を判定
-    if request.method == 'POST':
-        if request.session.get['correct_num'] == request.POST['key_value']:
-            return render(request, "keygame/game.html", {'right': 1})
+    if request.method == 'POST':    # POSTだった場合答え合わせをし、正解の場合rightをフラグにして送る
+        if request.session.get('correct_num') == int(request.POST['key_value']):
+            return render(request, "keygame/key_game.html", {'right': 1})
         else:
-            return render(request, 'keygame/game.html', {'right': -1})
-    else:
-        return render(request, 'keygame/access_failed.html')
+            return render(request, 'keygame/key_game.html', {})
+
+    return render(request, 'keygame/key_game.html')
